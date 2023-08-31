12c1a87610534aaf6883ce9ed1ad9ab0ab977fe7
Ahmed Tamim <ahmedtamim19050@gmail.com>
2023-08-31 14:42:27 +0600

    resume 1

---
+++ resources/views/pages/pdf2.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">\
+    <link rel="stylesheet" href="{{ asset('assets/pdf2.css') }}">
+    <title>Document</title>
+</head>
+<body>
+         <div>
+            <h1>Hello</h1>
+            <h1>I’m Your Name</h1>
+            <p>123 YOUR STREET
+               YOUR CITY, ST 12345
+               <br>
+               (123) 456-7890
+               <br>
+               NO_REPLY@EXAMPLE.COM
+               </p>
+         </div>
+
+         <div>
+            <h1>Skills</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, aspernatur?</p>
+         </div>
+    <div>
+        <h1>Experience</h1>
+        <p>MONTH 20XX - PRESENT</p>
+        <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, laboriosam.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, laboriosam.</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, laboriosam.</li>
+        </ul>
+    </div>
+</body>
+</html>
